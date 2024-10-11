49bb3951b22b35433ba1b3620e89597d38d310cb
KyungsooLee <jmtkd9196@gmail.com>
2024-10-12 00:49:50 +0900

    feat: #3 Game 엔티티 매핑

---
+++ src/main/java/com/Eungbyeonmuga/RookieKBO/domain/game/entity/Game.java
@@ -0,0 +1,47 @@
+package com.Eungbyeonmuga.RookieKBO.domain.game.entity;
+
+import com.Eungbyeonmuga.RookieKBO.domain.score.entity.HomeAway;
+import com.Eungbyeonmuga.RookieKBO.domain.score.entity.Score;
+import com.Eungbyeonmuga.RookieKBO.domain.score.entity.ScoreType;
+import com.Eungbyeonmuga.RookieKBO.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLRestriction("deleted_at is null")
+public class Game extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "game_id")
+    private Long id;
+
+    @OneToMany(mappedBy = "id", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<Score> homeScores = new ArrayList<>();
+
+    @OneToMany(mappedBy = "id", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<Score> AwayScores = new ArrayList<>();
+
+    @Column(nullable = false)
+    private Inning inning;
+
+    @Column(nullable = false)
+    private Season season;
+
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+}
