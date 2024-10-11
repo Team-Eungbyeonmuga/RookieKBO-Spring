903b37cb5f5fc87176a02976fbce5e6212c04ca3
KyungsooLee <jmtkd9196@gmail.com>
2024-10-11 12:13:18 +0900

    feat: #1 스프링 프로젝트 생성

---
+++ src/test/java/com/Eungbyeonmuga/RookieKBO/RookieKboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eungbyeonmuga.RookieKBO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RookieKboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
